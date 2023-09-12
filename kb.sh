06f321e988a4bda58075008bf828745fe0f6d5fd
Wessel klein Snakenborg <100232604+wessel-novacustom@users.noreply.github.com>
2023-09-12 13:43:37 +0200

    Add files via upload

---
+++ kb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+apt install -y git dkms build-essential linux-headers-$(uname -r)
+rm -rf ~/tuxedo-keyboard
+rmmod clevo_acpi
+rmmod clevo_wmi
+rmmod tuxedo_io
+rmmod tuxedo_keyboard
+sudo pacman -S base-devel
+sudo pacman -S dkms
+pamac install $(pamac list --quiet --installed | grep "^linux[0-9]*[-rt]*$" | awk '{print $1"-headers"}' ORS=' ')
+dnf -y group install "C Development Tools and Libraries"
+dnf -y group install "Development Tools"
+dnf -y install git
+dnf -y install dkms
+dnf -y install kernel-headers
+dnf -y install kernel-devel
+git clone https://github.com/wessel-novacustom/clevo-keyboard
+cd tuxedo-keyboard/
+make clean
+sudo make dkmsinstall
+echo tuxedo_keyboard >> /etc/modules
+modprobe tuxedo_keyboard
+echo "options tuxedo_keyboard color=WHITE" > /etc/modprobe.d/tuxedo_keyboard.conf
+rm -rf ~/tuxedo-keyboard
+exit 0
